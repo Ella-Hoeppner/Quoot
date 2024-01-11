ea66596c426c0f8dac1f8e64ac0ca45390ca7662
Ella Hoeppner <ellahoeppner@gmail.com>
2024-01-11 18:44:00 -0500

    making number parsing tests into real tests

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 mod interpreter;
 mod parse;
 use interpreter::repl;
-use parse::test_parser;
 
 fn main() {
   //repl();
-  test_parser();
 }
--- src/parse.rs
+++ src/parse.rs
@@ -7,19 +7,28 @@ use pest_derive::Parser;
 #[grammar = "core_lisp.pest"]
 pub struct CoreLispParser;
 
-pub fn test_parser() {
+#[test]
+fn parse_numbers() {
   let valid_nums = ["1", "-1", "1.0", "-273.15", "50", "5.", ".0"];
   let invalid_nums = ["hi!!", "a5", "5a", "", ".", "5.0.0", ".0.0", "0..1"];
 
   println!("testing valids...");
   valid_nums.map(|num_string| {
     let successful_parse = CoreLispParser::parse(Rule::number, num_string);
-    println!("\n{:?}\n", successful_parse);
+    assert!(
+      successful_parse.is_ok(),
+      "{} shouldn't parse as a valid number!",
+      num_string
+    );
   });
 
   println!("testing invalids...");
   invalid_nums.map(|num_string| {
     let successful_parse = CoreLispParser::parse(Rule::number, num_string);
-    println!("\n{:?}\n", successful_parse);
+    assert!(
+      successful_parse.is_err(),
+      "{} should parse as a valid number!",
+      num_string
+    );
   });
 }
